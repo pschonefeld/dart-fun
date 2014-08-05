60c006a00855eec1dc6fd8e1a61a77f35c4e7509
Peter Schonefeld <peter.schonefeld@gmail.com>
2014-08-06 02:06:25 +1000

    custom elements work ok
    project running with all files as local imports (no named library)

--- troubleshooting/polymer_debug/web/test_a.dart
+++ troubleshooting/polymer_debug/web/test_a.dart
@@ -1,10 +1,8 @@
 import 'package:polymer/polymer.dart';
-import 'test_b.dart';
 import 'test_c.dart';
+
 @CustomTag('test-a')
 class TestA extends PolymerElement {
-
-  var temp = new TestB.created();
   TestA.created() : super.created() {
   }
 
--- troubleshooting/polymer_debug/web/test_c.dart
+++ troubleshooting/polymer_debug/web/test_c.dart
@@ -1,8 +1,6 @@
 import 'package:polymer/polymer.dart';
 import 'mymixin.dart';
-/**
- * A Polymer click counter element.
- */
+
 @CustomTag('test-c')
 class TestC extends PolymerElement with MyMixin{
 
